084b0ba423a9d5fd61791f357c2613ddc7ca778a
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-11-19 23:40:34 +0900

    add 404 page

---
+++ src/components/pages/Page404.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'react'
+
+export const Page404: FC = () => <h1>404</h1>
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import { Page404 } from '~/components/pages/Page404'
+
 export const Router: FC = () => (
   <Routes>
     <Route path="/" />
-    <Route path="*" />
+    <Route path="*" element={<Page404 />} />
   </Routes>
 )
