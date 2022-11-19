91edcf81207bde344e5354b11abd821e0a16ea7d
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2022-11-19 18:10:28 +0900

    eslint, pretter, huskyのセットアップ完了

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
+  resolve: {
+    alias: [{ find: '~', replacement: '/src' }]
+  },
   plugins: [react()]
 })
