cc378abf7bd0c1bc2a94061376e2f534cf9af80c
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2023-01-14 18:49:59 +0900

    レイアウトと、一部処理の修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 
 import { useGetLabels } from '~/hooks/useGetLabels'
 
@@ -25,52 +25,55 @@ export const Home: FC = () => {
   } = useGetLabels()
 
   return (
-    <div className="mx-auto max-w-7xl">
+    <div className="container mx-auto">
       <h1 className="text-3xl font-bold">mv labels</h1>
-      <hr />
+      <hr className="mb-2" />
       <div className="flex gap-5">
         <div className="basis-1/2">
           <div>
             <h2 className="text-xl font-bold">export</h2>
             <div className="mb-6 gap-6">
               <div className="mb-3">
-                <label className="mb-2 block text-sm font-medium text-slate-900">owner</label>
+                <label className="mb-1 block text-sm font-medium text-slate-900">owner</label>
                 <input
-                  className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500"
+                  className="block h-8 w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   onChange={event => setExportOwner(event.target.value)}
                   value={exportOwner}
                 />
               </div>
               <div className="mb-3">
-                <label className="mb-2 block text-sm font-medium text-slate-900">repo</label>
+                <label className="mb-1 block text-sm font-medium text-slate-900">repo</label>
                 <input
-                  className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500"
+                  className="block h-8 w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   onChange={event => setExportRepo(event.target.value)}
                   value={exportRepo}
                 />
               </div>
               <div className="mb-3">
-                <label className="mb-2 block text-sm font-medium text-slate-900">token</label>
+                <label className="mb-1 block text-sm font-medium text-slate-900">token</label>
                 <input
-                  className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500"
+                  className="block h-8 w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   onChange={event => setExportToken(event.target.value)}
                   value={exportToken}
                 />
               </div>
               <button
-                className="shrink-0 cursor-pointer rounded-lg border border-slate-300 bg-slate-50 py-2 px-6 font-bold"
-                onClick={() => getExportLabels({ owner: exportOwner, repo: exportRepo, token: exportToken })}
+                className="shrink-0 cursor-pointer rounded-lg border border-slate-300 bg-slate-50 py-1 px-6 font-bold disabled:cursor-not-allowed disabled:border-slate-200 disabled:text-slate-500 disabled:shadow-none"
+                disabled={exportOwner === '' || exportRepo === '' || exportToken === ''}
+                onClick={useCallback(() => {
+                  getExportLabels({ owner: exportOwner, repo: exportRepo, token: exportToken })
+                }, [exportOwner, exportRepo, exportToken])}
               >
                 Get Label
               </button>
             </div>
           </div>
-          {exportLabels.length === 0 ? (
-            <p>ラベルはありません</p>
-          ) : isExportError ? (
-            <p>エラーが発生しました</p>
+          {isExportError ? (
+            <p className="text-red-500">エラーが発生しました</p>
           ) : isExportLoading ? (
             <p>Loding...</p>
+          ) : exportLabels.length === 0 ? (
+            <p>ラベルはありません</p>
           ) : (
             <div>
               <div className="text-sm font-medium text-slate-900">エクスポートするラベル</div>
@@ -106,43 +109,46 @@ export const Home: FC = () => {
             <h2 className="text-xl font-bold">import</h2>
             <div className="mb-6 gap-6">
               <div className="mb-3">
-                <label className="mb-2 block text-sm font-medium text-slate-900">owner</label>
+                <label className="mb-1 block text-sm font-medium text-slate-900">owner</label>
                 <input
-                  className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500"
+                  className="block h-8 w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   onChange={event => setImportOwner(event.target.value)}
                   value={importOwner}
                 />
               </div>
               <div className="mb-3">
-                <label className="mb-2 block text-sm font-medium text-slate-900">repo</label>
+                <label className="mb-1 block text-sm font-medium text-slate-900">repo</label>
                 <input
-                  className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500"
+                  className="block h-8 w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:outline-none focus:ring-1 focus:ring-blue-500"
                   onChange={event => setImportRepo(event.target.value)}
                   value={importRepo}
                 />
               </div>
               <div className="mb-3">
-                <label className="mb-2 block text-sm font-medium text-slate-900">token</label>
+                <label className="mb-1 block text-sm font-medium text-slate-900">token</label>
                 <input
-                  className="block w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:ring-blue-500"
+                  className="block h-8 w-full rounded-lg border border-slate-300 bg-slate-50 p-2.5 text-sm text-slate-900 focus:border-blue-500 focus:outline-none focus:ring-1  focus:ring-blue-500"
                   onChange={event => setImportToken(event.target.value)}
                   value={importToken}
                 />
               </div>
               <button
-                className="shrink-0 cursor-pointer rounded-lg border border-slate-300 bg-slate-50 py-2 px-6 font-bold"
-                onClick={() => getImportLabels({ owner: importOwner, repo: importRepo, token: importToken })}
+                className="shrink-0 cursor-pointer rounded-lg border border-slate-300 bg-slate-50 py-1 px-6 font-bold disabled:cursor-not-allowed disabled:border-slate-200 disabled:text-slate-500 disabled:shadow-none"
+                disabled={importOwner === '' || importRepo === '' || importToken === ''}
+                onClick={useCallback(() => {
+                  getImportLabels({ owner: importOwner, repo: importRepo, token: importToken })
+                }, [importOwner, importRepo, importToken])}
               >
                 Get Label
               </button>
             </div>
           </div>
-          {importLabels.length === 0 ? (
-            <p>ラベルはありません</p>
-          ) : isImportError ? (
-            <p>エラーが発生しました</p>
+          {isImportError ? (
+            <p className="text-red-500">エラーが発生しました</p>
           ) : isImportLoading ? (
             <p>Loding...</p>
+          ) : importLabels.length === 0 ? (
+            <p>ラベルはありません</p>
           ) : (
             <div>
               <div className="text-sm font-medium text-slate-900">インポート先に残っているラベル</div>
