bd20afd598ce4ab7bd9b02c18c5579750a03763d
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2023-01-15 01:51:26 +0900

    fix amplify setting

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mvlabels4f796ef3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
