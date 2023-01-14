e724d53aefd944971eee2e083d2e2f9b89189f25
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2023-01-15 05:53:08 +0900

    fix

--- src/hooks/useTracking.ts
+++ src/hooks/useTracking.ts
@@ -6,7 +6,7 @@ export const usePageTracking = (): void => {
   const location = useLocation()
 
   useEffect(() => {
-    ReactGA.initialize(import.meta.env.GA4_MEASUREMENT_ID)
+    ReactGA.initialize(`${import.meta.env.GA4_MEASUREMENT_ID}`)
     ReactGA.send({
       hitType: 'pageview',
       page: location.pathname + location.search
