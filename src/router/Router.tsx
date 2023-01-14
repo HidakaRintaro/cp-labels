4bee6164b053f85d468ccfe5053b6c41881d3dcb
HidakaRintaro <53255166+HidakaRintaro@users.noreply.github.com>
2023-01-15 05:36:51 +0900

    fix

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,10 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 
 import { Home } from '~/components/pages/Home'
 import { Page404 } from '~/components/pages/Page404'
+import { usePageTracking } from '~/hooks/useTracking'
 
-export const Router = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<Page404 />} />
-  </Routes>
-)
+export const Router = () => {
+  usePageTracking()
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  )
+}
